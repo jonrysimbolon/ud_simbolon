a7faca82c8b68dcdfb98e2693c309df4f6c281db
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-17 08:34:09 +0700

    adding snackbar function

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,16 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 var bgColor = const Color(0xFF80C764);
 var cardColor = const Color(0xFF72BD55);
 var textColor = const Color(0xFF9F9D9D);
+
+void showCustomSnackBar(BuildContext context, String message, Duration duration) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(message),
+      duration: duration,
+    ),
+  );
+}
