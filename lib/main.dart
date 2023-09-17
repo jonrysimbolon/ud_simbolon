8aff41c48e8d246762dfa2420b0ba2ce2add8062
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-17 19:38:24 +0700

    make color appbar and body same

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'UD. SIMBOLON',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: bgColor),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Home'),
@@ -38,7 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
 
-    // Add a listener to the controller to check for text changes
     _controller.addListener(() {
       setState(() {
         _isNotEmpty = _controller.text.isNotEmpty;
@@ -55,38 +54,38 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: HeaderComponent(
-          controller: _controller,
-          isNotEmpty: _isNotEmpty,
+        appBar: AppBar(
+          backgroundColor: bgColor,
+          title: HeaderComponent(
+            controller: _controller,
+            isNotEmpty: _isNotEmpty,
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 18,
+                child: IconButton(
+                    onPressed: () {
+                      showAlert(context, _controller.text);
+                    },
+                    icon: const Icon(
+                      Icons.search,
+                      color: cardColor,
+                    )),
+              ),
+            )
+          ],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 18,
-              child: IconButton(
-                  onPressed: () {
-                    showAlert(context, _controller.text);
-                    //showCustomSnackBar(context, _controller.text);
-                  },
-                  icon: const Icon(
-                    Icons.search,
-                    color: cardColor,
-                  )),
+        body: Column(
+          children: [
+          Container(
+            color: bgColor,
+            child: const Center(
+              child: Text("hey"),
             ),
           )
-        ],
-      ),
-      body: const Center(
-          child: Text(
-        'Custom Font',
-        style: TextStyle(
-          fontSize: 30,
-        ),
-      )),
-    );
+        ]));
   }
 }
