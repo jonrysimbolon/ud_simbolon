8a875a24575f5f50c37d5751402f78706e2338b5
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-18 16:26:15 +0700

    progress for promo

---
+++ lib/model/promo.dart
@@ -0,0 +1,17 @@
+class Promo {
+  String title;
+  String pathImg;
+
+  Promo({required this.title, required this.pathImg});
+}
+
+var promos = [
+  Promo(
+    title: 'Beli 3 buku gratis 1 pulpen',
+    pathImg: 'images/imageslider.jpeg',
+  ),
+  Promo(
+    title: 'Beli 3 buku gratis 1 pulpen',
+    pathImg: 'images/imageslider.jpeg',
+  ),
+];
--- lib/promo_component.dart
+++ lib/promo_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ud_simbolon/model/promo.dart';
 import 'package:ud_simbolon/utils.dart';
 
 class PromoComponent extends StatelessWidget {
@@ -6,10 +7,18 @@ class PromoComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return itemPromo('Beli 3 buku gratis 1 pulpen', 'images/imageslider.jpeg',
-        () {
-      showAlert(context, 'Beli 3 buku gratis 1 pulpen');
-    });
+    return SizedBox(
+      height: 200, // Adjust the height as needed
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: promos.length,
+        itemBuilder: (BuildContext context, int index) {
+          final promo = promos[index];
+          return itemPromo(
+              promo.title, promo.pathImg, () => {showAlert(context, promo.title)});
+        },
+      ),
+    );
   }
 }
 
@@ -20,25 +29,40 @@ Widget itemPromo(
 ) {
   return InkWell(
     onTap: click,
-    child: Container(
-      width: 100,
-      height: 50,
-      decoration: BoxDecoration(
-          color: cardColor, borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        children: [
-          Image.asset(
-            pathImg,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              title,
-              style: const TextStyle(
-                  fontFamily: fontTitleStr, fontSize: 12, color: Colors.white),
+    child: Padding(
+      padding: const EdgeInsets.only(left: 10, bottom: 10),
+      child: Container(
+        width: 200,
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              child: Image.asset(
+                pathImg,
+                fit: BoxFit.cover,
+              ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontFamily: fontTitleStr,
+                  fontSize: 12,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     ),
   );
